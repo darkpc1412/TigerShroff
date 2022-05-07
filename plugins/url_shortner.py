9eb914c2d09b8e76fa8208b18582af8a8777b6d6
darkpc1412 <102814997+darkpc1412@users.noreply.github.com>
2022-05-07 21:13:27 +0530

    Update url_shortner.py

--- plugins/url_shortner.py
+++ plugins/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join Movie Group', url='https://telegram.me/cinemaenglish123')
+        InlineKeyboardButton(text='join Movie Group', url='https://t.me/mnxmovies124')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\nmade by @mnxmovies124 | @mnxmovies124 🔥"
         return shorten_urls
     except Exception as error:
         return error
